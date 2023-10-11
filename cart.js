6c017520342cbc7a8bbceff4ff634b3cff20e430
okellojr <147662570+okellojr@users.noreply.github.com>
2023-10-12 00:41:13 +0300

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,31 @@
+function showCartTable() {
+	var cartRowHTML = "";
+	var itemCount = 0;
+	var grandTotal = 0;
+
+	var price = 0;
+	var quantity = 0;
+	var subTotal = 0;
+
+	if (sessionStorage.getItem('shopping-cart')) {
+		var shoppingCart = JSON.parse(sessionStorage.getItem('shopping-cart'));
+		itemCount = shoppingCart.length;
+
+		//Iterate javascript shopping cart array
+		shoppingCart.forEach(function(item) {
+			var cartItem = JSON.parse(item);
+			price = parseFloat(cartItem.price);
+			quantity = parseInt(cartItem.quantity);
+			subTotal = price * quantity
+
+			cartRowHTML += "<tr>" +
+				"<td>" + cartItem.productName + "</td>" +
+				"<td class='text-right'>$" + price.toFixed(2) + "</td>" +
+				"<td class='text-right'>" + quantity + "</td>" +
+				"<td class='text-right'>$" + subTotal.toFixed(2) + "</td>" +
+				"</tr>";
+
+			grandTotal += subTotal;
+		});
+	}
+}
